7b484ed74a8019d49d407b20fd92582788298df7
muradab <se.murad.abdella@gmail.com>
2021-09-09 12:12:33 -0700

    edited the login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,7 +1,7 @@
 import 'package:covid_vaccination/bloc/form_submission.dart';
-import 'package:covid_vaccination/bloc/login_bloc.dart';
-import 'package:covid_vaccination/bloc/login_event.dart';
-import 'package:covid_vaccination/bloc/login_state.dart';
+import 'package:covid_vaccination/bloc/login/login_bloc.dart';
+import 'package:covid_vaccination/bloc/login/login_event.dart';
+import 'package:covid_vaccination/bloc/login/login_state.dart';
 import 'package:covid_vaccination/repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
