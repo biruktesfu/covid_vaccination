3369d0c468eb2c26ff3455fabb9966a8e0e39454
muradab <se.murad.abdella@gmail.com>
2021-09-09 13:33:46 -0700

    structured the frontend and backend

---
+++ frontend/lib/repository/auth_repo_signup.dart
@@ -0,0 +1,8 @@
+class AuthRepositorysignup {
+  Future<void> login() async {
+    print("attempting signup");
+    Future.delayed(Duration(seconds: 2));
+
+    print("Signup successful");
+  }
+}
--- lib/repository/auth_repo_signup.dart
+++
@@ -1,8 +0,0 @@
-class AuthRepositorysignup {
-  Future<void> login() async {
-    print("attempting signup");
-    Future.delayed(Duration(seconds: 2));
-
-    print("Signup successful");
-  }
-}
