2bb90ec466c5d9bd67bf415feb4bd78b413201f1
biruk <it.biruk.tesfaye@gmail.com>
2021-09-09 13:05:18 +0300

    login form validation done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:covid_vaccination/login.dart';
+import 'package:covid_vaccination/repository/authentication_repository.dart';
+import 'package:covid_vaccination/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: login());
+    return MaterialApp(
+      home: RepositoryProvider(
+        create: (context) => AuthRepository(),
+        child: login(),
+      ),
+    );
   }
 }
