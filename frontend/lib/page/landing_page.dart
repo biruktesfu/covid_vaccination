75e3a6ed2e40aa5953d4d7265497446e45da479f
Mathewos Worku <se.mathewos.worku@gmail.com>
2021-09-12 00:25:27 +0300

    landing page made dynamic

--- frontend/lib/page/landing_page.dart
+++ frontend/lib/page/landing_page.dart
@@ -3,23 +3,49 @@ import 'dart:ui';
 import 'package:covid_vaccination/screens/login.dart';
 import 'package:covid_vaccination/screens/signup.dart';
 import 'package:flutter/material.dart';
+import '../widget/info_card.dart';
+
+// Data
+import '../data/card_data.dart';
+
+// Model
+import '../models/card_model.dart';
 
 class LandingPage extends StatelessWidget {
   static const String routeName = '/landingPage';
+  final List<CardCatagory> _categories = categories;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        _image(),
-        ListView(
-          padding: EdgeInsets.only(top: 50, left: 20.0, right: 20.0),
-          children: [
-            introCard(context),
-            infoCard(),
-          ],
-        ),
-      ]),
+      body: Stack(
+        children: [
+          _image(),
+          ListView(
+            children: [
+              introCard(context),
+              Container(
+                height: 800.0,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 0, left: 20.0, right: 20.0),
+                  child: ListView.builder(
+                    itemCount: _categories.length,
+                    itemBuilder: (
+                      BuildContext context,
+                      int index,
+                    ) {
+                      return InfoCard(
+                          cardTitle: _categories[index].cardTitle,
+                          subTitle: _categories[index].subTitle,
+                          description: _categories[index].description);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -47,32 +73,32 @@ class LandingPage extends StatelessWidget {
     );
   }
 
-  Widget infoCard() {
-    return Container(
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.arrow_drop_down_circle),
-              title: const Text('Covid Update'),
-              subtitle: Text(
-                'New variant - Delta',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'The Delta variant is more contagious: The Delta variant is highly contagious, more than 2x as contagious as previous variants.Some data suggest the Delta variant might cause more severe illness than previous variants in unvaccinated people. In two different studies from Canada and Scotland, patients infected with the Delta variant were more likely to be hospitalized than patients infected with Alpha or the original virus that causes COVID-19. Even so, the vast majority of hospitalization and death caused by COVID-19 are in unvaccinated people.',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  // Widget infoCard() {
+  //   return Container(
+  //     child: Card(
+  //       clipBehavior: Clip.antiAlias,
+  //       child: Column(
+  //         children: [
+  //           ListTile(
+  //             leading: Icon(Icons.arrow_drop_down_circle),
+  //             title: const Text('Covid Update'),
+  //             subtitle: Text(
+  //               'New variant - Delta',
+  //               style: TextStyle(color: Colors.black.withOpacity(0.6)),
+  //             ),
+  //           ),
+  //           Padding(
+  //             padding: const EdgeInsets.all(16.0),
+  //             child: Text(
+  //               'The Delta variant is more contagious: The Delta variant is highly contagious, more than 2x as contagious as previous variants.Some data suggest the Delta variant might cause more severe illness than previous variants in unvaccinated people. In two different studies from Canada and Scotland, patients infected with the Delta variant were more likely to be hospitalized than patients infected with Alpha or the original virus that causes COVID-19. Even so, the vast majority of hospitalization and death caused by COVID-19 are in unvaccinated people.',
+  //               style: TextStyle(color: Colors.black.withOpacity(0.6)),
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 
   Widget _image() {
     return Container(
